f41f89cf0c74f70ed89d461173d58bd992e468bf
Scott Campbell <stc9486@gmail.com>
2021-02-16 18:39:20 -0500

    client pack, public bundle

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-	return <div>i am Home</div>;
+	return (
+		<div>
+			<div>i am Home</div>
+			<button onClick={() => console.log('Hi')}>Press Me</button>
+		</div>
+	);
 };
 
 export default Home;
