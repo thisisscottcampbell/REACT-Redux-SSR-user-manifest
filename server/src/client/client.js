766391ec13f153c93206f273fe44576d1b29964a
Scott Campbell <stc9486@gmail.com>
2021-02-16 21:47:38 -0500

    client root

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,1 +1,5 @@
-console.log('Hi from client.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.render(<Home />, document.querySelector('#root'));
